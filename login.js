608ca788a200c25f0e4e4b052554e026a9efbcb1
Plutão <129420223+plutoandprogramation@users.noreply.github.com>
2023-06-05 16:23:56 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,7 @@
 var email = document.getElementById("email")
 var senha = document.getElementById("senha")
 var loginBtn = document.getElementById("gerador")
+var ent = 1
 
 loginBtn.addEventListener("click", ()=> {
     if (email.value != "" && senha.value != ""){
@@ -8,5 +9,16 @@ loginBtn.addEventListener("click", ()=> {
 }
 else {
         alert("Digite um e-mail / senha")
+    }
+});
+
+document.addEventListener("keypress", (event)=> {
+    if (event.key == "Enter") {
+        if (email.value != "" && senha.value != ""){
+            window.location.href = 'homepage.html'
+    }
+    else {
+            alert("Digite um e-mail / senha")
+        }
     }
 });
